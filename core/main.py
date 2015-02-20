5bdccec7320f5dfb2a4a2f52ff61f4848642b0c7
capic <vcapic@gmail.com>
2015-02-20 19:13:35 +0100

    little correction

--- core/main.py
+++ core/main.py
@@ -103,7 +103,7 @@ def prepare_files(directory, files_to_scan, episode_to_download_in_account):
                     # files_to_scan.append(file_infos)
 
     for dir in dirs:
-        prepare_files(directory + dir, files_to_scan, episode_to_download_in_account)
+        prepare_files(directory + "/" + dir, files_to_scan, episode_to_download_in_account)
 
     xbmc.log(settings.LOG_ADDON_NAME + "*** OUT prepare_files", xbmc.LOGDEBUG)
 
@@ -163,7 +163,7 @@ def main():
             progress.close()
             progress.create("Prepare files to mark as download. Please wait...")
             for directory in settings.VIDEO_SOURCES:
-                files_to_scan = prepare_files(directory + "/", files_to_scan, episode_to_download_in_account)
+                files_to_scan = prepare_files(directory, files_to_scan, episode_to_download_in_account)
 
             xbmc.log(settings.LOG_ADDON_NAME + "-------- list files %s (%s)" % (files_to_scan, len(files_to_scan)),
                      xbmc.LOGNOTICE)
